ca8f36195f072c1705d9e739064892a2b73639de
Concedo <39025047+LostRuins@users.noreply.github.com>
2025-10-13 14:50:47 +0800

    try fix cu11 fa

--- ggml/src/ggml-cuda/fattn-wmma-f16.cuh
+++ ggml/src/ggml-cuda/fattn-wmma-f16.cuh
@@ -27,7 +27,7 @@ static bool ggml_cuda_should_use_wmma_fattn(const int cc) {
 #if defined(GGML_USE_HIP) && !defined(GGML_HIP_ROCWMMA_FATTN)
     return false;
 #else
-    if ((GGML_CUDA_CC_IS_NVIDIA(cc) && ggml_cuda_highest_compiled_arch(cc) == GGML_CUDA_CC_VOLTA) ||
+    if ((GGML_CUDA_CC_IS_NVIDIA(cc) && ggml_cuda_highest_compiled_arch(cc) >= GGML_CUDA_CC_VOLTA) ||
         GGML_CUDA_CC_IS_RDNA3(cc) || GGML_CUDA_CC_IS_MTHREADS(cc)) {
         return true;
     } else if (GGML_CUDA_CC_IS_CDNA(cc)){
